56f149c59afe1ef011eb36bb3a17a4862cd474e4
zakariabidouli <zbidouli@e2r4p8.1337.ma>
2023-03-22 14:01:36 +0000

    verify map

--- map_ctrl.c
+++ map_ctrl.c
@@ -2,34 +2,47 @@
 
 // check if map is enclosed by walls wall = 1
 
+void edge(char *line)
+{
+    int j;
+
+    j = -1;
+    while (++j < ft_strlen(line))
+    {
+        if (line[j] == '0')
+            cub3d_error("Map is not closed/surrounded by walls.\n");
+    }
+
+}
+
+void    sides(char *line)
+{
+    printf("{%s}",line);
+        
+}
+
 void check_map(t_cube   *info)
 {
-    int     i;
-    int     j;
-
-    i = 0;
-    while (i < info->map_height)
+    int i;
+    int j;
+    if (info->map[0][0] && info->map[info->map_height - 1][0])
     {
-
-        if ( i == 0 || i == info->map_width)
-        {
-            j = 0;
-            while (j++ < info->map_width)
-            {
-                printf("map[0][%d] = [%c]\n", j, info->map[0][j]);
-                if (info->map[0][j] == '0' || info->map[info->map_height - 1][j] == '0')
-                    cub3d_error("Map is not closed/surrounded by walls.\n");
-            }
-            // cub3d_error("Map is not closed/surrounded by walls.\n");
-        }
-        i++;
+        edge(&info->map[0][0]);
+        edge(&info->map[info->map_height - 1][0]);
     }
-    // i = 0;
-    // while (i < info->map_width)
-    // {
-    //     if (info->map[i][0] != '1' || info->map[i][info->map_height - 1] != '1')
-    //         cub3d_error("Map is not closed/surrounded by walls.\n");
-    //     i++;
-    // }
+    i = -1;
+    while (++i < info->map_height && info->map[i][0])
+    {
+        j = -1; 
+        while(++j < info->map_width && info->map[i][j])
+        {
+            if (info->map[i][j] && info->map[i][j] == '0')
+            {
+                sides(&info->map[i][0]);
+                break;
+            }
+
+        }
+    }
 }
 
